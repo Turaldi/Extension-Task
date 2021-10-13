13394dc43eb4baeadfbe701db07ecebef2ebcd9f
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-14 00:28:17 +0400

    Extension Task

---
+++ Extension Task/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_Task
+{
+    public static class Extension
+    {
+        public static int Task(this string name1,string name2)
+        {
+            string[] arr = name1.Split(" ");
+            int count = 0;
+            foreach (var item in arr)
+            {
+                if (item==name2)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Extension Task/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Extension_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Beyim nasilsin uzun zaman oldu goruseli";
+            string name2 = "Beyim";
+            Console.WriteLine(name1.Task(name2));
+        }
+    }
+}
